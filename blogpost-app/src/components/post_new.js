e553a98c5d7346e0a7a0d82e0d2fbf07cf6d303b
João Olivio Neto <joaoolivioneto@gmail.com>
2017-09-08 14:16:16 -0300

    Blogpost App

---
+++ blogpost-app/src/components/post_new.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostNew extends Component {
+	renderField (field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					{ ...field.input }
+					type="text" 
+					placeholder="" 
+				/>
+				{field.meta.error}
+			</div>
+		)
+	}
+
+	render () {
+		return (
+			<form>
+				<Field 
+					label="Title"
+					name="title"
+					component={this.renderField}
+				/>
+				<Field 
+					label="Categories"
+					name="categories"
+					component={this.renderField}
+				/>
+				<Field 
+					label="Post Content"
+					name="content"
+					component={this.renderField}
+				/>			
+			</form>
+		);
+	}
+}
+
+function validate (values) {
+	const errors = {};
+
+	if(!values.title || values.title.length < 3) {
+		errors.title = "Enter a title that is at least 3 characters!";
+	}
+	if(!values.categories) {
+		errors.categories = "Enter some categories!";
+	}
+	if(!values.content) {
+		errors.content = "Enter some content please!";
+	}
+
+	return errors;
+}
+
+export default reduxForm({
+	validate,
+	form:'PostsNewForm'
+})(PostNew);
